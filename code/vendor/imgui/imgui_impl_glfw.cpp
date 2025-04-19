cadeeeaebb0c562204d23360de945c4f9f7d8b40
situlsrtq <seanohagan4@gmail.com>
2025-04-18 18:28:50 -0700

    fixes for GLFW path in vendor library

--- code/vendor/imgui/imgui_impl_glfw.cpp
+++ code/vendor/imgui/imgui_impl_glfw.cpp
@@ -105,20 +105,20 @@
 #endif
 
 // GLFW
-#include <GLFW/glfw3.h>
+#include "../vendor/GLFW/glfw3.h"
 
 #ifdef _WIN32
 #undef APIENTRY
 #ifndef GLFW_EXPOSE_NATIVE_WIN32
 #define GLFW_EXPOSE_NATIVE_WIN32
 #endif
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
+#include "../vendor/GLFW/glfw3native.h"   // for glfwGetWin32Window()
 #endif
 #ifdef __APPLE__
 #ifndef GLFW_EXPOSE_NATIVE_COCOA
 #define GLFW_EXPOSE_NATIVE_COCOA
 #endif
-#include <GLFW/glfw3native.h>   // for glfwGetCocoaWindow()
+#include "../vendor/GLFW/glfw3native.h"   // for glfwGetCocoaWindow()
 #endif
 #ifndef _WIN32
 #include <unistd.h>             // for usleep()
@@ -128,7 +128,7 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #ifdef EMSCRIPTEN_USE_PORT_CONTRIB_GLFW3
-#include <GLFW/emscripten_glfw3.h>
+#include "../vendor/GLFW/emscripten_glfw3.h"
 #else
 #define EMSCRIPTEN_USE_EMBEDDED_GLFW3
 #endif
