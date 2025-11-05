d432e95d16babf369fed097fde7f493b59a9b53b
situlsrtq <seanohagan4@gmail.com>
2025-11-05 15:01:47 -0800

    hash table work

--- src/util/u_hash.cpp
+++ src/util/u_hash.cpp
@@ -1,14 +1,68 @@
 #include "u_hash.h"
 
-void hash_table_t::Insert(void* key, int len, int32_t* value)
+void hash_table_t::Move(robin_node_t res, robin_node_t* frame)
+{
+	if(frame->flag == FLAG_AVAILABLE)
+	{
+		frame->key = res.key;	
+		frame->value = res.value;	
+		frame->displacement = res.displacement;	
+		frame->flag = FLAG_POPULATED;	
+		return;
+	}
+
+	frame++;
+	res.displacement++;
+
+	while(frame->displacement > res.displacement)
+	{
+		frame++;
+		res.displacement++;
+	}
+
+	if(frame->flag != FLAG_AVAILABLE)
+	{
+		robin_node_t temp = *frame;
+		frame->key = res.key;	
+		frame->value = res.value;	
+		frame->displacement = res.displacement;	
+		frame->flag = FLAG_POPULATED;	
+		Move(temp, frame++);
+		return;
+	}
+}
+
+void hash_table_t::Insert(void* key, int len, uint32_t value)
 {
 	uint32_t hashed_key;
 	MurmurHash3_x86_32(key, len, TABLE_SEED, &hashed_key);
 	hashed_key %= TABLE_SIZE;
 
-	if(table[hashed_key].flag == 0)
+	robin_node_t res = {key, value, 0, 0};
+	robin_node_t* frame = &table[hashed_key];
+
+	Move(res, frame);
+}
+
+uint32_t hash_table_t::Find(void* search_key, int len)
+{
+	uint32_t hashed_key;
+	MurmurHash3_x86_32(search_key, len, TABLE_SEED, &hashed_key);
+	hashed_key %= TABLE_SIZE;
+
+	uint32_t displacement = 0;
+	robin_node_t* frame = &table[hashed_key];
+
+	while(displacement <= frame->displacement)
 	{
-		table[hashed_key].value = *value;	
-		table[hashed_key].flag = FLAG_POPULATED;	
+		if(frame->key == search_key)
+		{
+			return frame->value; 
+		}
+
+		frame++;
+		displacement++;
 	}
+
+	return KEY_NOT_FOUND;
 }
--- src/util/u_hash.h
+++ src/util/u_hash.h
@@ -6,24 +6,31 @@
 #include<murmur/MurmurHash3.h>
 
 #define TABLE_SIZE 512
+#define KEY_NOT_FOUND TABLE_SIZE+1
 #define TABLE_SEED 42069
 
+#define FLAG_AVAILABLE 0
 #define FLAG_POPULATED 1
 
 struct robin_node_t
 {
-	int8_t flag;
+	void* key;
 	uint32_t value;
 	uint32_t displacement;
+	uint8_t flag;
 };
 
 struct hash_table_t
 {
 	robin_node_t table[TABLE_SIZE];
 
-	void Insert(void* key, int len, int32_t* value);
+	void Insert(void* key, int len, uint32_t value);
 	void Remove(void* key);
-	void Find(void* key);
+	uint32_t Find(void* search_key, int len);
+
+	private:
+
+	void Move(robin_node_t res, robin_node_t* frame);
 };
 
 #endif
